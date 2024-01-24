68ff4b97e92b695888455fa949ceb7176efd4ee5
Hetso <datacener23491248>
2024-01-25 00:31:07 +0500

    add layouts

---
+++ src/components/layout/AppSider.jsx
@@ -0,0 +1,52 @@
+import { Layout, Card, Statistic, List, Typography } from 'antd';
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+
+const siderStyle = {
+    padding: '1rem',
+};
+
+const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+];
+  
+
+export default function AppSider(){
+    return (
+    <Layout.Sider width="25%" style={siderStyle}>
+        <Card style={{ marginBottom: '1rem'}}>
+            <Statistic
+                title="Active"
+                value={11.28}
+                precision={2}
+                valueStyle={{ color: '#3f8600' }}
+                prefix={<ArrowUpOutlined />}
+                suffix="%"
+            />
+            <List
+            dataSource={data}
+            renderItem={(item) => (
+                <List.Item>
+                <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                </List.Item>
+            )}
+            />
+        </Card>
+        <Card>
+            <Statistic
+                title="Idle"
+                value={9.3}
+                precision={2}
+                valueStyle={{
+                    color: '#cf1322',
+                }}
+                prefix={<ArrowDownOutlined />}
+                suffix="%"
+            />
+        </Card>
+    </Layout.Sider>
+    )
+}
